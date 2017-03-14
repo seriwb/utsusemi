a86306fc0e50e0844f0da68fe1dcebd6ef6c4573
seri <seri.wb@gmail.com>
2017-03-15 08:39:32 +0900

    add spring-boot library

---
+++ src/main/java/box/white/utsusemi/Main.java
@@ -0,0 +1,23 @@
+package box.white.utsusemi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author seri
+ *
+ */
+public class Main {
+
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+
+		try {
+			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+
+}
